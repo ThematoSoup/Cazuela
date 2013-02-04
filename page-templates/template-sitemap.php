60b8d682f85a608814205964b07df7013a62ee51
Slobodan Manic <slobodan.manic@gmail.com>
2013-02-04 14:28:48 +0100

    Updated post header, added Sitemap page template

---
+++ page-templates/template-sitemap.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Template Name: Sitemap
+ *
+ * @package Cazuela
+ * @since Cazuela 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php do_action( 'thsp_before_content' ); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php get_template_part( 'post', 'lead' ); ?>
+				
+					<div class="entry-inner">
+						<?php get_template_part( 'post', 'header' ); ?>
+					
+						<div class="entry-content">
+							<?php the_content(); ?>
+							
+							<!-- Sitemap template code -->
+							<h2><?php _e( 'Pages', 'thsp_cazuela' ); ?></h2>
+							<ul id="sitemap-pages" class="sitemap-list"><?php wp_list_pages( 'title_li=' ); ?></ul>
+							
+							<h2><?php _e( 'Categories', 'thsp_cazuela' ); ?></h2>
+							<ul id="sitemap-categories" class="sitemap-list"><?php wp_list_categories( 'title_li=' ); ?></ul>
+
+							<h2><?php _e( 'Latest Posts', 'thsp_cazuela' ); ?></h2>
+							<ul id="sitemap-posts" class="sitemap-list">
+							<?php
+								// Get 25 latest posts
+								$args = array(
+									'posts_per_page' => 25
+								);
+								$recent_posts = get_posts( $args );
+								foreach( $recent_posts as $recent_post ) {
+									echo '<li><a href="' . get_permalink( $recent_post->ID ) . '" title="Permalink to  ' . esc_attr( $recent_post->post_title ) . '">' . $recent_post->post_title . '</a></li>';
+								}								
+							?>
+							</ul>
+							<!-- End sitemap template code -->
+							
+							
+							<?php edit_post_link( __( 'Edit', 'thsp_cazuela' ), '<span class="edit-link">', '</span>' ); ?>
+						</div><!-- .entry-content -->
+					</div><!-- .entry-inner -->
+				</article><!-- #post-<?php the_ID(); ?> -->
+
+				<?php comments_template( '', true ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+			<?php do_action( 'thsp_after_content' ); ?>
+
+		</div><!-- #content .site-content -->
+	</div><!-- #primary .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- post-header.php
+++ post-header.php
@@ -1,5 +1,9 @@
 <header class="entry-header">
-	<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'thsp_cazuela' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+	<?php if ( is_singular() ) { ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	<?php } else { ?>
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'thsp_cazuela' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+	<?php } ?>
 
 	<?php if ( 'post' == get_post_type() ) : ?>
 	<div class="entry-meta">
